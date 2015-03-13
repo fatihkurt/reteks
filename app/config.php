b981fe4da8d21fbeccb9291e5e6ffefd1422d79a
Fatih Kurt <fatih.kurt@liderlervadisi.com>
2015-03-13 18:10:10 +0200

    Config eklendi

---
+++ app/config.php
@@ -0,0 +1,19 @@
+<?php
+
+
+// Only invoked if mode is "production"
+$app->configureMode('production', function () use ($app) {
+    $app->config(array(
+        'log.enable'=> true,
+        'debug'     => false
+    ));
+});
+
+
+// Only invoked if mode is "development"
+$app->configureMode('development', function () use ($app) {
+    $app->config(array(
+        'log.enable'=> false,
+        'debug'     => true
+    ));
+});
