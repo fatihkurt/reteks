2f82dd795055de36f73672bd12f0e6930c1bf8e3
Fatih Kurt <fatih.kurt@liderlervadisi.com>
2015-04-05 18:12:56 +0300

    Upload fix

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -7,17 +7,19 @@ class IndexController extends ControllerBase
 
     public function index($lang='tr') {
 
-        $news = \App\Model\News::find(1)
-                    ->select('id')
-                    ->orderBy('start_date', 'desc')
-                    ->with(['contents' =>function($q)  {
+//         $news = \App\Model\News::find(1)
+//                     ->select('id')
+//                     ->orderBy('start_date', 'desc')
+//                     ->with(['contents' =>function($q)  {
 
-                        $q->select('description', 'title', 'seo_url');
+//                         $q->select('description', 'title', 'seo_url');
 
-                        $q->where('lang', '=', $this->lang);
-                    }])
-                    ->take(5)
-                    ->get();
+//                         $q->where('lang', '=', $this->lang);
+//                     }])
+//                     ->take(5)
+//                     ->get();
+
+        $news = [];
 
         $this->app->render('index.twig', [
 
